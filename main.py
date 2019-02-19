5d8c8d651dd362446888e3b5b53eb5a082151af9
Maarten Mol <maartenmol96@gmail.com>
2019-02-19 17:18:38 +0100

    dfhdf

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from pprint import pprint
 import json
 import sys
 
+def convert(data):
+    if isinstance(data, basestring):
+        return str(data)
+    elif isinstance(data, collections.Mapping):
+        return dict(map(convert, data.iteritems()))
+    elif isinstance(data, collections.Iterable):
+        return type(data)(map(convert, data))
+    else:
+        return data
+
 client = MongoClient()
 db = client.test
 customers = db.customers
@@ -11,5 +21,4 @@ coupons = db.coupons
 
 all_customers = customers.find()
 for i in all_customers:
-    #pprint(i)
-    json.dump(i)
+    pprint convert(i)
