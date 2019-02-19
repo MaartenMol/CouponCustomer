b43e43826dc2627ddfa5e443bef92e2f9cc1f113
Maarten Mol <maartenmol96@gmail.com>
2019-02-19 16:45:46 +0100

    9

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pymongo
-import pprint
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -7,8 +6,8 @@ db = client.test
 customers = db.customers
 coupons = db.coupons
 
-print('hoi')
-print(customers.find())
-pprint.pprint(db.customers.find().pretty())
-pprint.pprint(coupons.find())
+#print('hoi')
+#print(customers.find())
 
+all_customers = customers.find()
+print(all_customers)
