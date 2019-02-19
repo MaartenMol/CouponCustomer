32fc5d29aad4db539c26929a342237cb9a632cf3
Maarten Mol <maartenmol96@gmail.com>
2019-02-19 17:06:53 +0100

    test

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ db = client.test
 customers = db.customers
 coupons = db.coupons
 
-all_customers.encode('utf8') = customers.find()
+all_customers = customers.find()
 for i in all_customers:
-    pprint(i)
+    pprint(i).encode('utf8')
