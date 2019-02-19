a6db4f590c206735893ceaff6bae5d672c9d2614
Maarten Mol <maartenmol96@gmail.com>
2019-02-19 21:24:43 +0100

    test

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ db = client.test
 customers = db.customers
 coupons = db.coupons
 
+posts = db.posts
+
 #Convert a dict to UTF8
 def convert(data):
     if isinstance(data, basestring):
@@ -36,6 +38,13 @@ def searchCoupons(column, keyWord):
         i = convert(i)
         pprint(i)
 
+#Add coupons
+#Status 0 = unregistrered, 1 = registrered to user, 2 = used by user
+def addCoupons(id, status, value):
+    postData = { "id": id, "status": status, "value": value }
+    result = coupons.insert_one(postData)
+    print('Added Coupon: {0}'.format(result.inserted_id))
+
 #Print all customers
 #all_customers = customers.find()
 #for i in all_customers:
@@ -48,3 +57,5 @@ pprint("Zoeken naar alle ongeregistreerde coupons")
 searchCoupons("status", "0")
 pprint("Zoeken naar alle coupons van Jane")
 searchCoupons("customer_id", "5c6c1d49a990cde0ca31a23e")
+pprint("Voeg coupon toe")
+addCoupon("hyvgpHWenqM", "0", "1")
